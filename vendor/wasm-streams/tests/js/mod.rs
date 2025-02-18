c96454499769b17c785fd9a0792932321ca54b61
Arend-Jan Oosterveld <arendjan.oosterveld@gmail.com>
2025-02-18 14:23:09 +0700

    fix for developing offline

--- vendor/wasm-streams/tests/js/mod.rs
+++
@@ -1,7 +0,0 @@
-pub use readable_stream::*;
-pub use transform_stream::*;
-pub use writable_stream::*;
-
-mod readable_stream;
-mod transform_stream;
-mod writable_stream;
--- vendor/wiremock/src/http.rs
+++
@@ -1,3 +0,0 @@
-//! Convenient re-exports of http types that are part of `wiremock`'s public API.
-pub use http::{HeaderMap, HeaderName, HeaderValue, Method};
-pub use url::Url;
